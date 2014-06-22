d9df717f29914fa803181bbb832bbef7850b5b49
SIMSU <subins2000@gmail.com>
2014-06-22 20:50:54 +0530

    Initial Version

---
+++ readme.txt
@@ -0,0 +1,50 @@
+=== Plugin Name ===
+Contributors: subins2000
+Donate link: http://subinsb.com
+Tags: stats, popular, posts, statcounter
+Requires at least: 3.0.1
+Tested up to: 3.9
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: sc-popular-posts
+
+Displays Popular Posts From StatCounter stats as a widget. Only you have to do is make the stats public and give the project ID to this plugin.
+
+== Description ==
+
+Displays Popular Posts From StatCounter stats as a widget. Only you have to do is make the stats public and give the project ID to this plugin.
+
+You can configure which type of stats (pageviews, visitors) should be used to display the popular posts and no of items to be displayed.
+
+== Installation ==
+
+1. Upload `sc-popular-posts` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= Should I be a Paid User of StatCounter to use this plugin =
+
+NO. Any users that is on a free plan or paid plan can use this plugin without any problems.
+
+= Do I need to set my Stats to Public in StatCounter =
+
+In order to access the Popular Pages, the stats should be made Public. This can be done by the following:
+
+1. Go to your Project Config Page
+2. Choose "User/Public Access" option from the sidebar
+3. Under "Public Access", set the option to "All Stats are Public"
+
+= Does the stats get fetched every time a page is loaded =
+
+No, because if it does that, the page loading will be slow. So the stats are cached for 10 minutes. This will reduce the number of times the stats should be fetched.
+
+== Screenshots ==
+
+Screenshots will be soon available.
+
+== Changelog ==
+
+= 0.1 =
+* Initial Version
---
+++ sc-popular-posts.php
@@ -0,0 +1,209 @@
+<?
+/**
+ * Plugin Name: StatCounter Popular Posts
+ * Plugin URI: http://subinsb.com/ask/popular-posts
+ * Description: Display Popular Posts from StatCounter
+ * Version: 0.1
+ * Author: Subin Siby
+ * Author URI: http://subinsb.com
+ * License: GPLv3
+*/
+
+function SCPP_optPage(){
+ 	if(isset($_POST['SPPid']) && $_POST['SPPid']!=""){
+ 		update_option("SPPproject", $_POST['SPPid']);
+ 		echo '<div id="message" class="updated"><p>Saved Settings</p></div>';
+ 	}
+ 	$SPPid = get_option("SPPproject")=="" ? "" : get_option("SPPproject");
+?>
+	<h1>SC Popular Posts Options</h1>
+	<p>You only have to enter the project ID of your StatCounter Project</p>
+	<form action="" method="POST">
+		<input type="text" name="SPPid" value="<?echo $SPPid;?>" size="40" placeholder="The ID that starts with p" /><br/>
+		<button>Save Settings</button>
+	</form>
+	<h2>Example</h2>
+	<p>If the summary page URL of your project looks like this :</p>
+	<blockquote>http://statcounter.com/p1234567/summary/</blockquote>
+	<p>Then <b>p1234567</b> is your Project ID.</p>
+<?
+}
+
+function SCPP_adminMenu() {
+ 	add_submenu_page('options-general.php', __('StatCounter Popular Posts'), __('StatCounter Popular Posts'), 'manage_options', 'SCPP_admin', 'SCPP_optPage');
+}
+add_action('admin_menu', 'SCPP_adminMenu');
+
+$pluginRoot = realpath(dirname(__FILE__));
+require $pluginRoot."/simple_html_dom.php";
+
+class SPP extends WP_Widget {
+	
+	private $pluginRoot;
+	
+	/**
+	 * Sets up the widgets name etc
+	 */
+	public function __construct() {
+		parent::__construct(
+			'spp_widget', // Base ID
+			__('StatCounter Popular Posts', 'spp_widget'), // Name
+			array( 'description' => __( 'StatCounter Popular Posts', 'spp_widget' ), ) // Args
+		);
+		global $pluginRoot;
+		$this->pluginRoot = $pluginRoot;
+	}
+
+	/**
+	 * Outputs the content of the widget
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 */
+	public function widget( $args, $instance ) {
+		echo $args['before_widget'];
+		$instance['type']  = isset($instance['type']) ? $instance['type']   : "";
+		$instance['items'] = isset($instance['items']) ? $instance['items']+1 : 10+1;
+		
+		echo $args['before_title'] . "Popular Posts" . $args['after_title'];
+		
+		if($instance['type'] == "pageviews" || $instance['type']==""){
+			$this->makeList("/popular/?pageloads&perpage=" . $instance['items']);
+		}else{
+			$this->makeList("/popular/?visitors&perpage=" . $instance['items']);
+		}
+		
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Outputs the options form on admin
+	 *
+	 * @param array $instance The widget options
+	 */
+	public function form( $instance ) {
+		// outputs the options form on admin
+?>
+	<p>Should Popular Posts be made by Pageviews or Uniques Visitors ?</p>
+	<select class="widefat" id="<?php echo $this->get_field_id('type'); ?>" name="<?php echo $this->get_field_name( 'type' ); ?>">
+		<option value="pageviews" >Pageviews</option>
+		<option value="visits" <?echo $instance['type']=="visits" ? "selected":""?>>Visits</option>
+	</select>
+	<p>The number of items to be displayed</p>
+	<input type="number" id="<?php echo $this->get_field_id('items'); ?>" name="<?php echo $this->get_field_name( 'items' ); ?>" value="<? echo isset($instance['items']) ? $instance['items']:10;?>" />
+	<br/><br/>
+<?	
+	}
+
+	/**
+	 * Processing widget options on save
+	 *
+	 * @param array $new_instance The new options
+	 * @param array $old_instance The previous options
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['type'] = ( ! empty( $new_instance['type'] ) ) ? strip_tags( $new_instance['type'] ) : '';
+		$instance['items'] = is_numeric($new_instance['items']) ? $new_instance['items'] : 10;
+
+		return $instance;
+	}
+	
+	public function makeList($url){
+		global $wp_query;
+ 		global $wp;
+ 		global $wpdb;
+		
+		$SPPid  = get_option("SPPproject")=="" ? "" : get_option("SPPproject");
+		$output = "";
+		
+		/* The Site URL */
+		$siteURL		  = "http://subinsb.com/"; // get_home_url()
+		$siteURLParts = parse_url($siteURL);
+ 		$siteURL		  = $siteURLParts['host'].$siteURLParts['path'];
+		
+		/* Make the SC page URL */
+		$url  = "http://statcounter.com/". $SPPid . $url . "&guest=1";
+		
+		$html = $this->getPage($url);
+		$html = str_get_html($html);
+		
+		foreach($html->find("table.results td") as $tdElem){
+			$aElem = $tdElem->find("div.iw a[href]", 0);
+			if(isset($aElem->href)){
+				$href = $aElem->href;
+				$current_url = parse_url($href);
+ 				$current_url = $current_url['host'].$current_url['path'];
+ 				
+  				$slug		 	 = str_replace($siteURL, "", $current_url);
+  				
+  				/* Avoid Display of Home Page */
+  				if($slug!=""){
+					$sqlstr = $wpdb->prepare("SELECT wposts.ID, wposts.guid
+    					FROM $wpdb->posts wposts
+   					WHERE wposts.post_name LIKE %s", "%$slug%"
+   				);
+   				$results = $wpdb->get_results($sqlstr, ARRAY_N);
+   				if(isset($results[0][0])){
+   					$post_id = $results[0][0];
+   			
+   					$title	= get_the_title($post_id);
+						$output .= $this->makeListItem($title, $href);
+					}
+				}
+			}
+		}
+		if($output!=""){
+			echo '<ol style="list-style: decimal;">';
+				echo $output;
+			echo '</ol>';
+		}else{
+			echo "No Popular Posts Yet";
+		}
+	}
+	
+	public function getPage($url){
+		$root = $this->pluginRoot;
+		if(file_exists($root."/pop_data.txt") && file_exists($root."/pop_data.html")){
+			$info = unserialize(file_get_contents($root . "/pop_data.txt" ));
+			if(is_array($info)){
+				if($info['time'] < strtotime("-10 minutes") || $info['url'] != $url){
+					return $this->getPopularData($url);
+				}else{
+					return file_get_contents($root."/pop_data.html");
+				}
+			}else{
+				return $this->getPopularData($url);
+			}
+		}else{
+			return $this->getPopularData($url);
+		}
+	}
+	
+	public function getPopularData($url){
+		$root = $this->pluginRoot;
+		$html = file_get_contents($url);
+		if($html){
+			/* We add a serialize data because we don't want anyone to mess with the file, so that when we unserialize a correct Array will be produced */
+			file_put_contents($root . "/pop_data.txt", serialize(array(
+		 		"time" => time(),
+		 		"url"  => $url
+			)));
+			file_put_contents($root . "/pop_data.html", $html);
+		}else{
+			if(file_exists($root."/pop_data.html")){
+				$html = file_get_contents($root."/pop_data.html");
+			}else{
+				$html = "";	
+			}
+		}
+		return $html;
+	}
+	
+	/* Make an Item of list */
+	public function makeListItem($title, $url){
+		return '<li><a href="'.$url.'" title="'.$title.'">'.$title.'</a></li>';
+	}
+}
+
+add_action('widgets_init', create_function('', 'return register_widget(\'SPP\');'));
